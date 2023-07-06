db94a716ced28178d1123dce1fc5743a1f1b7f9c
poo0054 <poo0054@outlook.com>
2023-07-06 10:22:17 +0800

    init add jdbctest

---
+++ spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-data-jdbc/src/test/java/io/github/poo0054/jdbc/jdbcTest.java
@@ -0,0 +1,13 @@
+package io.github.poo0054.jdbc;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author zhangzhi
+ */
+@SpringBootTest
+@AutoConfigureTestDatabase
+public class jdbcTest {
+
+}
